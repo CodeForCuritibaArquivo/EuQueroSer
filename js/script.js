2c9ba5cc73b649efaf4e55510ff46b9eb8bea73e
matheusmabo <matheusmabo@gmail.com>
2016-05-06 23:37:57 -0300

    opt-in e style header
    adicionado o opt-in para registro de email e finalizado o style do
    header

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$ (function () {
+
+	var window = $(window);
+
+
+	$('section[data-type = "background"]').each (function () {
+		var bgobj = $(this);
+
+		$(window).scroll (function () {
+			var yPos = ($window.scrollTop() /bgobj.data ('speed'));
+
+			//
+
+			var coords = '50% ' + yPos + 'px';
+
+
+			//
+
+
+			$bgobj.css ({backgroundPosition: coords});
+		});
+	})
+
+
+});
